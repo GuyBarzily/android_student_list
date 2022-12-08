30d08dd1fb49ae821f72ec69912fa694048f2fe1
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-12-08 15:47:11 +0200

    init list row

--- app/src/main/java/com/example/androidsecondassignment/StudentRecyclerListActivity.java
+++ app/src/main/java/com/example/androidsecondassignment/StudentRecyclerListActivity.java
@@ -1,17 +1,33 @@
 package com.example.androidsecondassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.androidsecondassignment.model.Model;
+import com.example.androidsecondassignment.model.Student;
+
+import java.util.List;
+
 public class StudentRecyclerListActivity extends AppCompatActivity {
+    List<Student> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_recycler_list);
 
-        RecyclerView rv = findViewById(R.id.student_recycler_list);
+        data = Model.instance().getAllStudents();
+        RecyclerView list = findViewById(R.id.student_recycler_list);
+        list.setHasFixedSize(true);
+
+        list.setLayoutManager(new LinearLayoutManager(this));
+        //StudentRecyclerAdapter adapter = new StudentRecyclerAdapter();
+
+
+
+
     }
 }
