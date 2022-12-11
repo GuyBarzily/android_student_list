88ea3d6bc86dd164d8a74e49e2384853aba0441a
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-12-11 12:21:42 +0200

    changes to add and added second avatar

--- app/src/main/java/com/example/androidsecondassignment/StudentAddActivity.java
+++ app/src/main/java/com/example/androidsecondassignment/StudentAddActivity.java
@@ -6,18 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.androidsecondassignment.model.Model;
 import com.example.androidsecondassignment.model.Student;
 
-import java.util.List;
 
 public class StudentAddActivity extends AppCompatActivity {
     EditText id,name,address,phone;
     Button cancel_btn,save_btn;
     ActionBar actionBar;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,27 +29,32 @@ public class StudentAddActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Add Student");
 
-        id = (EditText) findViewById(R.id.student_add_id);
-        name = (EditText) findViewById(R.id.student_add_name);
-        address = (EditText) findViewById(R.id.student_add_address);
-        phone = (EditText) findViewById(R.id.student_add_phone);
+        id =  findViewById(R.id.student_add_id);
+        name =  findViewById(R.id.student_add_name);
+        address =  findViewById(R.id.student_add_address);
+        phone =  findViewById(R.id.student_add_phone);
+        imageView = findViewById(R.id.student_add_image);
+
+        imageView.setOnClickListener(view -> {
+            imageView.setImageResource(R.drawable.avatar2);
+        });
 
 
-        cancel_btn = (Button) findViewById(R.id.student_add_cancelBtn);
-        save_btn = (Button) findViewById(R.id.student_add_saveBtn);
+        cancel_btn =  findViewById(R.id.student_add_cancelBtn);
+        save_btn = findViewById(R.id.student_add_saveBtn);
 
         save_btn.setOnClickListener(view -> {
             Student st = new Student(name.getText().toString(),id.getText().toString(),"",
                     false,address.getText().toString(),phone.getText().toString());
 
             Model.instance().addStudent(st);
+
+
             this.finish();
 
         });
 
-        cancel_btn.setOnClickListener(view -> {
-            this.finish();
-        });
+        cancel_btn.setOnClickListener(view -> this.finish());
 
     }
 
